d39562fcf971ade41e077faeae0d332f02fe5824
Parker Moore <parkrmoore@gmail.com>
2012-09-10 15:40:56 -0400

    Added school_name and school_address to Nyym model.

--- app/models/nyym.rb
+++ app/models/nyym.rb
@@ -1,3 +1,3 @@
 class Nyym < ActiveRecord::Base
-  attr_accessible :age, :email, :has_paid, :name, :payment_received_on, :phone_number, :school, :teacher_email, :teacher_name, :voice_part
+  attr_accessible :age, :email, :has_paid, :name, :payment_received_on, :phone_number, :school_name, :school_address, :teacher_email, :teacher_name, :voice_part
 end
