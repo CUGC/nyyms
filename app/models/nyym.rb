41cc2ba7258cfe32c96fd04dba9fa7deedab04b6
Parker Moore <parkrmoore@gmail.com>
2012-09-10 22:52:45 -0400

    Added Nyym.voice_parts

--- app/models/nyym.rb
+++ app/models/nyym.rb
@@ -1,3 +1,13 @@
 class Nyym < ActiveRecord::Base
   attr_accessible :age, :email, :has_paid, :name, :payment_received_on, :phone_number, :school_name, :school_address, :teacher_email, :teacher_name, :voice_part
+  
+  def self.voice_parts
+    {
+      "Tenor 1" => "tenor1",
+      "Tenor 2" => "tenor2",
+      "Baritone" => "baritone",
+      "Bass" => "bass"
+    }
+  end
+  
 end
