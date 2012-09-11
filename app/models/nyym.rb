79997cce74241c3ab2d4171ea5e5782a3b5b49bf
Parker Moore <parkrmoore@gmail.com>
2012-09-11 00:50:34 -0400

    lol, not anymore

--- app/models/nyym.rb
+++ app/models/nyym.rb
@@ -2,7 +2,6 @@ class Nyym < ActiveRecord::Base
   attr_accessible :age, :email, :has_paid, :name, :payment_received_on, :phone_number, :school_name, :school_address, :teacher_email, :teacher_name, :voice_part
 
   validates_presence_of :age, :email, :name, :school_name, :school_address, :teacher_email, :teacher_name, :voice_part
-  validates_format_of :phone_number, :message => "must be a valid telephone number.", :with => lambda{ |nyym| nyym.phone_number.nil? ? true : /^[\(\)0-9\- \+\.]{10,20} *[extension\.]{0,9} *[0-9]{0,5}$/i } 
   validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/
   validates_format_of :teacher_email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/
   validates_inclusion_of :voice_part, :in => %w( tenor1 tenor2 baritone bass ), :message => "'%{value}' is not a possible option."
