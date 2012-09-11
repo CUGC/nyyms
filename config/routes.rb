7a064ce575be8e9c341a3600d0127c694bb94cac
Parker Moore <parkrmoore@gmail.com>
2012-09-10 21:52:25 -0400

    New route and taking account of new school fields

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Nyyms::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'nyyms#add'
+  root :to => 'nyyms#new'
 
   # See how all your routes lay out with "rake routes"
 
