5e09168dfdc163c1ed1e59db0b79750778782e32
Parker Moore <parkrmoore@gmail.com>
2012-09-30 13:52:03 -0400

    whitespace nonsense

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -3,5 +3,4 @@ class Registration < ActiveRecord::Base
   
   validates_presence_of :email, :name, :school_address, :school_name, :students_names, :students_voice_parts, :payment_plan
   validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/
-
 end
