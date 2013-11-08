61fef5b850bbc9bb1f9425006b4518da0a86b96b
Parker Moore <parkrmoore@gmail.com>
2013-11-07 23:44:34 -0500

    Use letter_opener for emails during local development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,8 +28,9 @@ Nyyms::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
-  
+
   # Specify host for ActionMailer
+  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = { :host => 'sage.dev' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
