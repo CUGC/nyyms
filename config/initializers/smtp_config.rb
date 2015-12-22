b08637db87ca194013db4cc0d4110e05126e9eb6
Parker Moore <parkrmoore@gmail.com>
2015-12-22 16:05:13 -0500

    Check for SENDGRID_USERNAME for better heroku support.

--- config/initializers/smtp_config.rb
+++ config/initializers/smtp_config.rb
@@ -10,6 +10,16 @@ if ENV['SMTP_HOST']
     :authentication       => ENV.fetch('SMTP_AUTH_METHOD', :plain).to_sym,
     :enable_starttls_auto => ENV.fetch('SMTP_ENABLE_STARTTLS', "true") == "true"
   }
+elsif ENV['SENDGRID_USERNAME']
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 else
   require 'yaml'
   stmp_settings_file = File.expand_path(
